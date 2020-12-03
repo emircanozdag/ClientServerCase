f210d256ed56b7fc9412933e9637e1216eba2a28
emircanozdag <68469546+emircanozdag@users.noreply.github.com>
2020-12-03 10:05:45 +0300

    .
    .

---
+++ ClientServer -- Client Script/ClientServer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace ClientServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string localAddress = LocalIPAdress();
+            //Console.WriteLine(localAddress);
+            //Console.ReadKey();
+            Socket listenSocket;
+            listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            int portNumber = 2085;
+            IPEndPoint localIPEndPoint = new IPEndPoint(IPAddress.Any, portNumber);
+            listenSocket.Bind(localIPEndPoint);
+            Console.WriteLine(" Server IP Address : " + LocalIPAdress());
+            Console.WriteLine(" Listenin on Port : " + portNumber);
+            listenSocket.Listen(4);
+            Socket acceptedSocket = listenSocket.Accept();
+            byte[] receiveBuffer = new byte[1024];
+            int receiveByteCount;
+            receiveByteCount = acceptedSocket.Receive(receiveBuffer, SocketFlags.None);
+            if (receiveByteCount > 0)
+            {
+                string message = Encoding.ASCII.GetString(receiveBuffer, 0, receiveByteCount);
+                Console.WriteLine(message);
+            }
+            acceptedSocket.Shutdown(SocketShutdown.Both);
+            acceptedSocket.Close();
+            Console.ReadLine();
+        }
+        public static string LocalIPAdress()
+        {
+            IPHostEntry host;
+            string localIP = "";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            return localIP;
+        }
+        
+    }
+}
---
+++ ClientServer -- Server Script/ClientServer -- Server Script/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServer____Server_Script
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverIP = "";
+            string serverPortNumber = "";
+            Console.Write("Enter Server IP Address : ");
+            serverIP = Console.ReadLine();
+            Console.WriteLine("Enter Server Port Number : ");
+            serverPortNumber = Console.ReadLine();
+            Console.Write("\n Sending to : " + serverIP + serverPortNumber);
+
+            Socket sendSocket;
+            sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress destinationIP = IPAddress.Parse(serverIP);
+            int destinationPortNumber = Int16.Parse(serverPortNumber);
+            IPEndPoint destinationEndPoint = new IPEndPoint(destinationIP, destinationPortNumber);
+            Console.WriteLine("\n Waiting to Connect ...");
+            sendSocket.Connect(destinationEndPoint);
+            Console.WriteLine("Connected...");
+            string message = GetMessage();
+            byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            Console.WriteLine("\nSending Data...");
+            sendSocket.Send(data, SocketFlags.None);
+            Console.WriteLine("Sending Complete...");
+            Console.ReadLine();
+
+
+
+
+        }
+
+        private static string GetMessage()
+        {
+            string message;
+            Console.WriteLine("\n Enter message to send : ");
+            message = Console.ReadLine();
+            return message;
+        }
+    }
+}
